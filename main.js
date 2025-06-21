05a70153d975144c766b9f3bb89228404a592217
Yahya K <128909790+yahiya4344@users.noreply.github.com>
2025-06-22 01:07:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var Typed = new Typed(".text",
+    {
+        strings: ["Frontend Developer", "Smartphone Engineer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    })
